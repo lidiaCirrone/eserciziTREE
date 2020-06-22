fb4aa9a73824e237cef64b811aa19feaa9eaeda7
Lidia Cirrone <lidia.cirrone@gmail.com>
2020-06-22 16:26:31 +0200

    Memory: fix shuffleColors()

--- memory/index.js
+++ memory/index.js
@@ -167,8 +167,8 @@ function generateTiles(cursor) {
 
 function shuffleColors(colors) {
    let j, x, i;
-   for (i = colors.length - 1; i > 0; i--) {
-      j = Math.floor(Math.random() * (i + 1));
+   for (i = colors.length - 1; i >= 0; i--) {
+      j = Math.floor(Math.random() * i) + 1;
       x = colors[i];
       colors[i] = colors[j];
       colors[j] = x;
